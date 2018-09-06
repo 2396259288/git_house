9a0ae72254ee001e10e524c7a01065cb92d92f1a
2396259288 <2396259288@qq.com>
2018-09-06 22:41:36 +0800

    在主页中加图片
    在model中增加img
    model法师变化要重新往数据库中添加
    在setting中进行设置 media_url 和 media_root
    在url.py 中导入地址包
    并且添加static 函数 找到图片url和文件路径
    在HTML中展示

--- gallery/models.py
+++ gallery/models.py
@@ -4,6 +4,15 @@ from django.db import models
 
 
 class Gallery(models.Model):
-	description = models.CharField(max_length=100)
+	description = models.CharField(default='介绍', max_length=100)
+	img = models.ImageField(default='default.png',upload_to='images/')
+	title = models.CharField(default='作品标题', max_length=20)
+	
+	def __str__(self):
+		return self.title
 
+
+
+    
+    
 
